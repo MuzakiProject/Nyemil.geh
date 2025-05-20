fc0fd3f0037ae9d965642105ed26530e80ac5f1a
Nur Hafiz Aulia <nurhafizaulia834@gmail.com>
2025-05-20 22:52:56 +0700

    menambahkan halaman, bantuan, dan panduan

--- Dashboard/index.php
+++ Dashboard/index.php
@@ -1,10 +1,10 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <title>Nyemil.geh - Dashboard</title>
+        <title>Nyemil.geh</title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css?v= <?php echo time(); ?>">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -13,41 +13,77 @@
     </head>
     <body>
         <?php
-        include "src/components/header.php";
-        ?>
-
-        <div class="d-flex">
-            <?php include "src/components/sidebar.php" ?>
-            <main class="bg-body-tertiary w-100 border-start">
-                <div class="contindex">
-                    <?php
-                    $page = $_GET['page'] ?? 0;
-                    switch ($page) {
-                        case 1:
-                            include __DIR__ . "/src/components/produk.php";
-                            break;
-                        case 2:
-                            include __DIR__ . "/src/components/pesanan.php";
-                            break;
-                        case 3:
-                            include __DIR__ . "/src/components/penjualan.php";
-                            break;
-                        case 4:
-                            include __DIR__ . "/src/components/user.php";
-                            break;
-                        default:
-                            include __DIR__ . "/src/components/main.php";
-                            break;
-                        }
-                        ?>
-                </div>
-                <?php include "src/components/footer.php";?>
-            </main>
-        </div>
+        session_start();
+        require __DIR__ . "/Dashboard/src/components/phplogic/database.php";
+        include __DIR__ . "/src/components/phplogic/function.php";
         
+        $footer = true;
+        $is_logged_in = isset($_SESSION['user']);
+            if ($is_logged_in) {
+                include __DIR__ . "/src/components/headerlog.php";
+            } else {
+                include __DIR__ . "/src/components/header.php";
+            }
+        ?>
+        
+        <main class=" w-100">
+            <div class="contindex">
+                <?php
+                $page = $_GET['page'] ?? 0;
+                switch ($page) {
+                    case 1:
+                        include __DIR__ . "/src/components/store.php";
+                        break;
+                    case 2:
+                        include __DIR__ . "/src/components/helpsupp.php";
+                        break;
+                    case 3:
+                        include __DIR__ . "/src/components/accsupp.php";
+                        break;
+                    case 4:
+                        include __DIR__ . "/src/components/aboutkami.php";
+                        break;
+                    case 5:
+                        include __DIR__ . "/src/components/detitems.php";
+                        break;
+                    case 6:
+                        $footer = false; 
+                        include __DIR__ . "/src/components/paybayar.php";
+                        break;
+                    case 7:
+                        include __DIR__ . "/src/components/profile.php";
+                        break;
+                    case 8:
+                        include __DIR__ . "/src/components/keranjang.php";
+                        break;
+                    case 9:
+                        include __DIR__ . "/src/components/ordetail.php";
+                        break;
+                    default:
+                        include __DIR__ . "/src/components/main.php";
+                        break;
+                }
+                ?>
+            </div>
+        </main>
+        <?php
+            footer($footer);
+        ?>
+        
+        <script src="src/components/js/navanim.js"></script>
+        <script src="src/components/js/counterplusmin.js"></script>
         <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
         <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.min.js" integrity="sha384-VQqxDN0EQCkWoxt/0vsQvZswzTHUVOImccYmSyhJTp7kGtPed0Qcx8rK9h9YEgx+" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/scrollreveal"></script>
+        <script>
+            ScrollReveal().reveal('.textmain, .prod-cover, .about-cover, .prom-cover, .store-cover, .brand-about, .Visi-misi', {
+                duration: 1000,
+                origin: 'bottom',
+                distance: '50px',
+                reset: false, // no repeat
+            });
+        </script>
     </body>
 </html>
