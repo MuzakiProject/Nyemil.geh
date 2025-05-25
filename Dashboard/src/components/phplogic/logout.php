9bbf803b083efcd93c2c9d072fcfc32baf5e6e0a
MuzakiProject <ahmadhosam2k@gmail.com>
2025-05-26 01:25:39 +0700

    fixlogout

--- Dashboard/src/components/phplogic/logout.php
+++ Dashboard/src/components/phplogic/logout.php
@@ -2,6 +2,7 @@
 session_start();
 session_unset(); // Menghapus semua variabel session
 session_destroy(); // Menghancurkan session
-
-header("Location: Dashboard/loginpage.php"); // Redirect ke halaman login
+echo "
+<script> window.location.href='/Dashboard/loginpage.php' </script>
+";
 exit();
